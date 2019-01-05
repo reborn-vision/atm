42ec837abb67e591d7513ea8cf54ce00d4de590b
Napvlm <hello@ruslananisimov.com>
2019-01-05 20:29:21 +0200

    preloader and buttons below

---
+++ preload.js
@@ -0,0 +1,4 @@
+$(window).on('load', function() { // makes sure the whole site is loaded
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
+});
